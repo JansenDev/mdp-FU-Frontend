71ae89bad5634b02666aec29aaf6d3a2675e0a89
cjmazzarri <cjmazzarri@gmail.com>
2022-03-29 12:45:38 -0500

    ✨Imported required modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TestpPipe } from './pipes/testp.pipe';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs'
 
 @NgModule({
   declarations: [TestpPipe],
-  imports: [CommonModule, MatSliderModule, MatCardModule],
-  exports: [TestpPipe, MatSliderModule, MatCardModule],
+  imports: [CommonModule, MatSliderModule, MatCardModule, MatButtonModule, MatTabsModule],
+  exports: [TestpPipe, MatSliderModule, MatCardModule, MatButtonModule, MatTabsModule],
 })
 export class SharedModule {}
