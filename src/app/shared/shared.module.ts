91779c0c255c3b300e5d198c6f430485c9eca6eb
Jhonatan <seguragjj25@gmail.com>
2022-03-29 21:06:11 -0500

    ADDED: resource map template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,26 @@ import { CommonModule } from '@angular/common';
 import { TestpPipe } from './pipes/testp.pipe';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [TestpPipe],
-  imports: [CommonModule, MatSliderModule, MatCardModule],
-  exports: [TestpPipe, MatSliderModule, MatCardModule],
+  imports: [CommonModule, MatSliderModule, MatCardModule, MatTableModule],
+  exports: [
+    TestpPipe,
+    MatSliderModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
+  ],
 })
 export class SharedModule {}
