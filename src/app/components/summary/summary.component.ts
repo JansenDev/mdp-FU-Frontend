fa1091e4a1b46a2f7fd6836149858bf5312678d0
Franccesco1907 <fjaimes@pucp.edu.pe>
2022-04-01 16:52:16 -0500

    Interface

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { TestService } from '../../core/services/test.service';
-
+import { SummaryService } from '../../core/services/summary.service';
+import { ISummaryResponse } from '../../core/models/summary.model';
 
 // declare var me : any;
 @Component({
@@ -12,42 +12,31 @@ import { TestService } from '../../core/services/test.service';
 })
 
 export class SummaryComponent implements OnInit {
-  
+  private summary: ISummaryResponse = {"customer":"Fallo","clm_effective": 0,"production": 0,"productivity": 0,"period": new Date(1648721412)};
   public customer: string = "";
   public clm_effective: number = 0;
   public production: number = 0;
   public productivity: number = 0;
   public period: Date = new Date();
-  public summary: any = {}; //{"data":{"customer":"Fallo","clm_effective":13.85,"production":99.99,"productivity":9999.99,"period":1648721412}};
+  // public summary: any = {}; //{"data":{"customer":"Fallo","clm_effective":13.85,"production":99.99,"productivity":9999.99,"period":1648721412}};
   
 
-  constructor(private service: TestService) {
+  constructor(private service: SummaryService) {
   }
   
 
   ngOnInit() {
     this.getSummary();
-  /*
-    this.summary = this.summary.data;
-
-    this.customer = this.summary.customer;
-    this.clm_effective = this.summary.clm_effective;
-    this.production = this.summary.production;
-    this.productivity = this.summary.productivity;
-    this.period = this.summary.period;
-  */  
   }
 
   getSummary() {
     this.service.getSummary().subscribe(data => {
       this.summary = data;
-      this.customer = data.data.customer;
-      this.clm_effective = data.data.clm_effective;
-      this.production = data.data.production;
-      this.productivity = data.data.productivity;
-      this.period = data.data.period;
-      console.log("data", data.data)
-      // return data;
+      this.customer = data.customer;
+      this.clm_effective = data.clm_effective;
+      this.production = data.production;
+      this.productivity = data.productivity;
+      this.period = data.period;
     }), (err: any) => {
       console.error(err);
     }
--- src/app/core/services/summary.service.ts
+++ src/app/core/services/summary.service.ts
@@ -3,20 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ISummaryResponse } from '../../core/models/summary.model';
 
-const URL = `https://rickandmortyapi.com/api/character/125`
 const summaryUrl = `https://62457c736b7ecf057c1ea959.mockapi.io/summary`;
 
 @Injectable({
   providedIn: 'root',
 })
-export class TestService {
+
+export class SummaryService {
   constructor(private httpClient: HttpClient) {}
-
-
-
-  findById(){
-    return this.httpClient.get<any>(URL)
-  }
   
   getSummary() : Observable<ISummaryResponse>{
     return this.httpClient.get<ISummaryResponse>(summaryUrl);
