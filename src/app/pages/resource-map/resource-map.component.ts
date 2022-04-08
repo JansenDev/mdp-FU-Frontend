8ea1b2f329a685910b7685b862dabdb38daccab4
cjmazzarri <cjmazzarri@gmail.com>
2022-04-08 03:41:32 -0500

    period test

--- src/app/pages/resource-map/resource-map.component.ts
+++ src/app/pages/resource-map/resource-map.component.ts
@@ -149,7 +149,7 @@ export class ResourceMapComponent implements OnInit {
     this.cod_colaborador = parseInt(resourceMapItem.cod_colaborador);
     this.cod_mapa_recurso = parseInt(resourceMapItem.cod_mapa_recurso);
     this.resourceDetailComponent.loadProductivity(this.cod_mapa_recurso);
-    this.resourceDetailComponent.loadContract(this.cod_colaborador, this.periodo);
+    this.resourceDetailComponent.loadContract(this.cod_colaborador, this.periodoToSummary);
     this.resourceDetailComponent.loadAssignments(this.cod_colaborador, this.periodo, this.cod_cliente);
     console.log("cod cliente: ",this.cod_cliente)
 
