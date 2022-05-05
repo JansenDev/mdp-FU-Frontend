654c96834313997c4625d0d648846e2edd078e57
Franccesco1907 <fjaimes@pucp.edu.pe>
2022-05-05 14:00:21 -0500

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { setToken } from 'src/app/core/utils/token.storage';
 
 @Component({
@@ -7,7 +8,7 @@ import { setToken } from 'src/app/core/utils/token.storage';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {
+  constructor(private router : Router) {
     //* ['DELIVERY_MANAGER', 'PROJECT_MANAGER', 'RRHH', 'ANALYST', 'GG']
     // let user_profile = localStorage.getItem("user");
     // if(user_profile != null) 
@@ -15,6 +16,8 @@ export class HomeComponent implements OnInit {
     // setToken({id_sesion: 40, "userProfile": user_profile})
     // setToken({user_profile})
     // setToken({ id_sesion: 40, userProfile: 'RRHH' });
+    if(!localStorage.getItem("jwt"))
+      router.navigate(['/login']);
   }
 
   ngOnInit(): void {}
