acb52de01d9348fe0cc60c16cc031308e6a26584
cjmazzarri <cjmazzarri@gmail.com>
2022-05-27 10:52:13 -0500

    ✨Added a new role

--- src/app/core/constants/profile.authorization.ts
+++ src/app/core/constants/profile.authorization.ts
@@ -25,6 +25,13 @@ export const authorizations: IAuthorizations = {
     'contract-imbox/approveHiringRequestComponent',
   ],
   ANALYST: [],
+  JEFE_DE_SERVICIOS: [
+    ...BASIC_ROUTES,
+    'resources',
+    'hiring-request',
+    'service-map',
+    'services-configuration',
+  ],
 };
 
 export interface IAuthorizations {
@@ -33,4 +40,5 @@ export interface IAuthorizations {
   JEFE_DE_RECURSOS_HUMANOS: string[];
   GERENTE_GENERAL: string[];
   ANALYST: string[];
+  JEFE_DE_SERVICIOS: string[];
 }
